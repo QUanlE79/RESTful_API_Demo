24c623d1b0aadbff84c643c8b9bed0237902ac12
lequan <lequan2002.ql@gmail.com>
2023-10-17 12:18:48 +0700

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import _ from "./config/config.js";
 import express from "express";
 import morgan from "morgan";
 import actorRoute from "./routes/actor.route.js"
-
+import cors from "cors"
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
