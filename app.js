56341f4bd81edcce4ff3e8be42a5ac4a527e85b8
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2023-10-23 21:30:02 +0700

    add more ways to import swagger schema file

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import cors from "cors";
 import swaggerJSDoc from "swagger-jsdoc";
 import SwaggerUi from "swagger-ui-express";
 import swaggerOption from './docs/swaggerOption.js'
-
+// import swaggerDocJSON from './docs/swaggerOption.json'  assert { type: 'json' };
+// import jsYaml from 'js-yaml'
+// import fs from 'fs'
 const app = express();
 
 
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 const swaggerDocs = swaggerJSDoc(swaggerOption);
+//const swaggerOptionYAML = jsYaml.load(fs.readFileSync("./docs/swaggerOption.yaml", "utf8"));
+
 app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
 
 app.get('/', (req,res)=>{
